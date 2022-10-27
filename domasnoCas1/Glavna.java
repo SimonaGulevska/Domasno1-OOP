7c781394a6fc34786f8cf9affdbfeb6eea77c8aa
SimonaGulevska <simonagulevska4@yahoo.com>
2022-10-27 19:09:12 +0200

    Dodavanje na linkot do repozitoriumot

--- domasnoCas1/Glavna.java
+++ domasnoCas1/Glavna.java
@@ -4,6 +4,6 @@ public class Glavna {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World OOP FIKT");
-		
+		System.out.println("Link do repozitorium: https://github.com/SimonaGulevska/Domasno1-OOP");
 	}
 }
